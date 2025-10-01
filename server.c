a0a2b6a684ee7d26c4cfae1451e09087f77f2097
Henrique Wolf <henriquewolf1999@gmail.com>
2025-10-01 08:42:08 -0300

    keep server setup in a separate function

--- server.c
+++ server.c
@@ -7,9 +7,6 @@
 #define PORT 502
 #define BUF_SIZE 128
 
-#define MSB 1
-#define LSB 0
-
 #define LSBYTE(x) ((x << 8) & 0xFF)
 #define MSBYTE(x) ((x) & 0xFF)
 
@@ -24,14 +21,10 @@ struct ModbusFrame {
     unsigned char *data;
 };
 
-int main() {
-    int server_fd, new_socket;
+int server_setup() {
+    int server_fd;
     struct sockaddr_in address;
     int opt = 1;
-    int addrlen = sizeof(address);
-
-    short registers[2200] = {0};
-    memset(registers, 12, sizeof(registers));
 
     // Create socket fd
     if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -64,6 +57,20 @@ int main() {
 
     printf("Server listening on port %d\n", PORT);
 
+    return server_fd;
+}
+
+int main() {
+    int server_fd = server_setup();
+    int new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    short registers[2200] = {0};
+    memset(registers, 15, sizeof(registers));
+    registers[8] = 300;
+    registers[9] = 200;
+
     while(1){
         struct ModbusFrame packet;
 
@@ -73,65 +80,57 @@ int main() {
         packet.unit_id = 1;
 
         // Memory allocation for data section
-        packet.data = (unsigned char *)malloc(119 * sizeof(unsigned char));
+        //packet.data = (unsigned char *)malloc(119 * sizeof(unsigned char));
         
-        packet.transac_id = 0;
+        //packet.transac_id = 0;
         packet.func_code = 3;
         packet.length = 7;
 
-        unsigned char *transac_id_ptr = (unsigned char *)&packet.transac_id;
-        unsigned char *prot_id_ptr = (unsigned char *)&packet.prot_id;
-        unsigned char *length_ptr = (unsigned char *)&packet.length;
-
         // Accept connections
         if((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("Connection error");
+            printf("Connection failed");
         }
         else {
             printf("Connection accepted\n");
+            while(1){
+                // Allocate memory for buffers, since the data package length varies
+                unsigned char *buff_recv = (unsigned char *)malloc(BUF_SIZE * sizeof(unsigned char));
+                unsigned char *buff_sent = (unsigned char *)malloc(BUF_SIZE * sizeof(unsigned char));
+                //buff_sent[0] = transac_id_ptr[MSB];
+                //buff_sent[1] = transac_id_ptr[LSB];
+                buff_sent[2] = (unsigned char)(MSBYTE(packet.prot_id));
+                buff_sent[3] = (unsigned char)(LSBYTE(packet.prot_id));
+                buff_sent[4] = (unsigned char)(MSBYTE(packet.length));
+                buff_sent[5] = (unsigned char)(LSBYTE(packet.length));
+                buff_sent[6] = packet.unit_id;
+                buff_sent[7] = packet.func_code;
+                buff_sent[8] = 0x04;
+                _ssize_t bytes_recv;
+                if((bytes_recv = read(new_socket, buff_recv, BUF_SIZE)) > 0) {
+                    buff_sent[0] = buff_recv[0];
+                    buff_sent[1] = buff_recv[1];
+                    buff_sent[9] = (unsigned char)(LSBYTE(registers[buff_recv[9]]));
+                    buff_sent[10] = (unsigned char)(MSBYTE(registers[buff_recv[9]]));
+                    buff_sent[11] = (unsigned char)(LSBYTE(registers[buff_recv[9+1]]));
+                    buff_sent[12] = (unsigned char)(MSBYTE(registers[buff_recv[9+1]]));
 
-            // Allocate memory for buffers, since the data package length varies
-            unsigned char *buff_recv = (unsigned char *)malloc(BUF_SIZE * sizeof(unsigned char));
-            unsigned char *buff_sent = (unsigned char *)malloc(BUF_SIZE * sizeof(unsigned char));
-            //buff_sent[0] = transac_id_ptr[MSB];
-            //buff_sent[1] = transac_id_ptr[LSB];
-            buff_sent[2] = prot_id_ptr[MSB];
-            buff_sent[3] = prot_id_ptr[LSB];
-            buff_sent[4] = length_ptr[MSB];
-            buff_sent[5] = length_ptr[LSB];
-            buff_sent[6] = packet.unit_id;
-            buff_sent[7] = packet.func_code;
-            buff_sent[8] = 0x04;
-            //buff_sent[9] = 0x00;
-            //buff_sent[10] = 0x25;
-            //buff_sent[11] = 0x00;
-            //buff_sent[12] = 0x20;
-            _ssize_t bytes_recv;
-            if((bytes_recv = read(new_socket, buff_recv, BUF_SIZE)) > 0) {
-                buff_sent[0] = buff_recv[0];
-                buff_sent[1] = buff_recv[1];
-                buff_sent[9] = (unsigned char)(LSBYTE(registers[buff_recv[9]]));
-                buff_sent[10] = (unsigned char)(MSBYTE(registers[buff_recv[9]]));
-                buff_sent[11] = (unsigned char)(LSBYTE(registers[buff_recv[9+1]]));
-                buff_sent[12] = (unsigned char)(MSBYTE(registers[buff_recv[9+1]]));
-
-                printf("Client message: 0x");
-                for(int i = 0; i < bytes_recv; i++){
-                    printf("%02X ", (unsigned char)buff_recv[i]);
+                    printf("Client message: 0x");
+                    for(int i = 0; i < bytes_recv; i++){
+                        printf("%02X ", (unsigned char)buff_recv[i]);
+                    }
+                    printf("\n");
+                    printf("Server response: 0x");
+                    for(int i = 0; i <= 12; i++){
+                        printf("%02X ", (unsigned char)buff_sent[i]);
+                    }
+                    printf("\n");
+                    send(new_socket, buff_sent, 13, 0);
                 }
-                printf("\n");
-                printf("Server response: 0x");
-                for(int i = 0; i <= 12; i++){
-                    printf("%02X ", (unsigned char)buff_sent[i]);
-                }
-                printf("\n");
-                send(new_socket, buff_sent, 13, 0);
-                packet.transac_id++;
+                free(buff_sent);
+                free(buff_recv);
+                //free(packet.data);
             }
-            free(buff_sent);
-            free(buff_recv);
         }
-        free(packet.data);
     }
 
     return 0;
